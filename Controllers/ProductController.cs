1ac45ebfab192cf973d99e22091c65ad7a506335
Deyvid Jaguaribe de Lira <deyvidjlira@dvitsolutions.com>
2019-09-17 22:07:06 -0300

    Create Product Model / Rename action Visualizar for Detsail

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,10 +1,23 @@
+using ecommerce_aspnet.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ecommerce_aspnet.Controllers {
     public class ProductController : Controller {
-        public ActionResult Visualizar() {
-            return View();
+        public ActionResult Detail() {
+            Product product = GetProduct();
+
+            return View(product);
             //return new ContentResult() { Content = "<h3>Produto visualizar</h3>", ContentType = "text/html" };
+        }
+
+
+        private Product GetProduct() {
+            return new Product() {
+                Id  = 1,
+                Name = "Xbox one X",
+                Description = "Jogue em 4k",
+                Value = 2000.00M
+            };
         }
     } 
 }
---
+++ Models/Product.cs
@@ -0,0 +1,8 @@
+namespace ecommerce_aspnet.Models {
+    public class Product {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Value { get; set; }
+    }
+}
