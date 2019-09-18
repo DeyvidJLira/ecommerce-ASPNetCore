3b3f5a462e7f44e01cc20b5cc36ab2d7b78f7db2
Deyvid Jaguaribe de Lira <deyvidjlira@dvitsolutions.com>
2019-09-17 21:19:28 -0300

    Create view Visualizar of Product

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace ecommerce_aspnet.Controllers {
     public class ProductController : Controller {
         public ActionResult Visualizar() {
-            return new ContentResult() { Content = "<h3>Produto visualizar</h3>", ContentType = "text/html" };
+            return View();
+            //return new ContentResult() { Content = "<h3>Produto visualizar</h3>", ContentType = "text/html" };
         }
     } 
 }
