2fc5d4ce95c0d28972727a11200c87e9ee5c5170
Deyvid Jaguaribe de Lira <deyvidjlira@dvitsolutions.com>
2019-09-22 12:01:35 -0300

    Create Contact View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,10 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ecommerce_aspnet.Controllers {
     public class HomeController : Controller {
-
         public IActionResult Index() {
             return View();
         }
-        
+        public IActionResult Contact() {
+            return View();
+        }
     }
 }
