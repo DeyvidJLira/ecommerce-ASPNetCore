f51911bd6721b5221d69e7b8bd9e3244949f6bc8
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-09-24 09:17:50 -0300

    Bug fix of return of  ContactAction / Implement validation of Contact

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,10 +2,12 @@ using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using ecommerce_aspnet.Libraries.Email;
 using ecommerce_aspnet.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace ecommerce_aspnet.Controllers {
     public class HomeController : Controller {
@@ -18,17 +20,30 @@ namespace ecommerce_aspnet.Controllers {
         public IActionResult ContactAction() {
             try {
                 Contact contact = new Contact(HttpContext.Request.Form["name"], HttpContext.Request.Form["email"], HttpContext.Request.Form["message"]);
-                bool result = EmailService.SendContact(contact);
-                if(result)
-                    ViewData["MSG_S"] = "Mensagem de contato enviada com sucesso!";
-                else
-                    ViewData["MSG_E"] = "Ops! Tivemos um erro, tente novamente mais tarde! Cód. 15";
+                
+                var listMessagesValidation = new List<ValidationResult>();
+                var context = new ValidationContext(contact);
+                bool isValid = Validator.TryValidateObject(contact, context, listMessagesValidation, true);
+
+                if(isValid) {
+                    bool result = EmailService.SendContact(contact);
+                    if(result)
+                        ViewData["MSG_S"] = "Mensagem de contato enviada com sucesso!";
+                    else
+                        ViewData["MSG_E"] = "Ops! Tivemos um erro, tente novamente mais tarde! Cód. 15";
+                } else {
+                    StringBuilder messages = new StringBuilder();
+                    foreach(var message in listMessagesValidation) {
+                        messages.Append(message.ErrorMessage);
+                    }
+                    ViewData["MSG_E"] = messages.ToString();
+                }
             } catch (Exception e) {
                 ViewData["MSG_E"] = "Ops! Tivemos um erro, tente novamente mais tarde! Cód. 05";
             }
             
 
-            return View("Content");
+            return View("Contact");
         }
         public IActionResult Login() {
             return View();
--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ecommerce_aspnet.Models {
     public class Contact {
         public Contact(string name, string email, string message) {
@@ -6,8 +8,18 @@ namespace ecommerce_aspnet.Models {
             this.Message = message;
         }
         
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
         public string Name {get; set;}
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(10)]
         public string Message { get; set; }
     }
 }
